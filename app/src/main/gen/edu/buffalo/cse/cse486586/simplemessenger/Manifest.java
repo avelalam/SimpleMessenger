64a7fa92a44fecf0dab7b25c237dd4cf89b18473
abv <abhilash5402@gmail.com>
2017-05-19 23:28:54 -0400

    First commit

---
+++ app/src/main/gen/edu/buffalo/cse/cse486586/simplemessenger/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package edu.buffalo.cse.cse486586.simplemessenger;
+
+/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
+public final class Manifest {
+}
